2c8db784a91698d9aad099621c3d9e3cbfffc9a6
henry1479 <fokys.81@mail.ru>
2021-06-05 22:30:48 +0300

    lesson-3.2

--- Основы программирования/advancedJS-1/lesson-3/main.js
+++ Основы программирования/advancedJS-1/lesson-3/main.js
@@ -39,8 +39,8 @@ class GoodsList {
 
     fetchGoods() {
         this.goods = [
-            {product_name: 'Ноутбук', id_product: 123 , price: 46500 },
-            {product_name: 'Мышка', id_product: 456 , price: 1000 },
+            { product_name: 'Ноутбук', id_product: 123, price: 46500 },
+            { product_name: 'Мышка', id_product: 456, price: 1000 },
         ]
     }
     // метод наполняте карточку содержимым и внедряет ее в html
@@ -63,18 +63,19 @@ class GoodsList {
     }
 }
 
-class CartItem extends GoodsItem{
-    constructor(id,title,price) {
-        // this.id = id;
-        // this.title = title;
-        // this.price = price;
+
+
+
+
+class CartItem extends GoodsItem {
+    constructor(id, title, price) {
         super(id, title, price);
     }
 
     render() {
-         
+
         return `<div class="cart__goods-item" itemId="${this.id}"><h3>${this.title}</h3><p>${this.price}</p> <button type="button" class="remove-btn">Remove from cart</button></div>`;
-        
+
     }
 
 }
@@ -83,6 +84,7 @@ class CartItem extends GoodsItem{
 class Cart {
     constructor() {
         this.cartGoods = [];
+        this.removeButtons = [];
     }
 
     // добавляет товары в виртуальную корзину по клику на кнопе в карточке
@@ -98,14 +100,44 @@ class Cart {
             }
         }
         this.renderCart();
-        
+        this.removeButtons = document.querySelectorAll('.remove-btn');
+        this.removeFromCart(this.removeButtons);
+
     }
 
 
 
     // удаляет товары из корзины по клику на кнопку на странице корзины "удалить"
-    removeFromCart() {
-        
+    removeFromCart(buttons) {
+        function test(event) {
+            for (let i = 0; i < document.querySelectorAll('.cart__goods-list').length; i++) {
+                const product = event.target.parentElement;
+                console.log(product); 
+                console.log(document.querySelectorAll('.cart__goods-list')[i].innerHTML); 
+                console.log(String(product.outerHTML) === String(document.querySelectorAll('.cart__goods-list')[i].innerHTML)); 
+                if (String(product.outerHTML) === String(document.querySelectorAll('.cart__goods-list')[i].innerHTML)) {
+                    this.cartGoods = this.cartGoods.slice(i);
+                    console.log(this.cartGoods);
+
+                }
+                this.renderCart();  
+            }
+            
+            
+            // проходимся циклом по массиву с товарами
+            // for (let item of this.cartGoods) {
+            //     if (product === item) { // если id кликнутой карточки и товара в массиве совпадают
+            //         const k = this.cartGoods.indexOf(item);
+            //         this.cartGoods = this.cartGoods.slice(k);
+            //         console.log(k);
+                    
+            //         // добавляем товар в корзину
+            //     }
+            // }
+            this.renderCart();
+
+        }
+        buttons.forEach(el => el.addEventListener('click', test.bind(this)))
     }
 
     // считает стоимость товаров в корзине
@@ -118,10 +150,10 @@ class Cart {
     renderCart() {
         let listHTML = '';
         this.cartGoods.forEach(good => {
-            const item  = new CartItem(good.id_product, good.product_name, good.price)
-             // новый объект
+            const item = new CartItem(good.id_product, good.product_name, good.price)
+            // новый объект
             listHTML += item.render();// добавляем в пустую строку карточку наполненную содержимым
-           
+
         })
 
         document.querySelector('.cart .cart__goods-list').innerHTML = listHTML;// внедряем в html
@@ -143,15 +175,13 @@ const init = () => {
     goodsList.render(); // отрисовываем карточки
 
     const cart = new Cart();
-    
-    const addBtn = document.querySelectorAll('.add-btn');
-    // вешаем обработчик на кнопки в карточке и привязываем к this класса
-    addBtn.forEach((el) => { el.addEventListener('click', cart.addToCart.bind(cart)) });
-    cartItem = new CartItem();
-    
 
-    const cardCart = new CartItem();
-    cardCart.render();
+    const addBtn = document.querySelectorAll('.add-btn');
+    // вешаем обработчик на кнопки в карточке и привязываем к this объекта
+    addBtn.forEach((el) => { el.addEventListener('click', cart.addToCart.bind(cart)) });
+    cart.removeFromCart(cart.removeButtons);
+
+
 
 }
 
