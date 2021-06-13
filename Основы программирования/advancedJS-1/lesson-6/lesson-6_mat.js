7e072045f11860a0f7f936425f6c02045cbeef09
henry1479 <fokys.81@mail.ru>
2021-06-13 22:56:39 +0300

    lesson-6.1

--- Основы программирования/advancedJS-1/lesson-6/lesson-6_mat.js
+++ Основы программирования/advancedJS-1/lesson-6/lesson-6_mat.js
@@ -3,31 +3,66 @@
 const API_URL =
     "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 
+//компонет поля поиска товаров
+Vue.component('search-input', {
+    //входной параметр для правильной работы v-model
+    props: ['value'],
+    template: `
+        <input type="text" class="goods-search"  v-bind:value="value"
+        v-on:input = "$emit('input', $event.target.value)"/>`
+})
+
+// компонент для кнопки поиска
+Vue.component('search-button', {
+    //входной параметр для метода filterGoods()
+    //которая является обработчиком на событие клика
+    props: ['filtergoods'],
+    template: `<button class="search-button" v-on:click="filtergoods" type="button">Искать</button>`
+})
+
+// компонент для блока с товарами
 Vue.component('goods-list', {
-    props: ['goods'],
-    template: ` <div class="goods-list">
-        <goods-item v-for="goodEntity in goods" :goodProp="goodEntity"></goods-item>
+    props: ['goods', 'addtocart'],
+    template: `<div class="goods-list">
+        <goods-item v-for="goodEntity in goods" :goodProp="goodEntity"  :id="goodEntity.id_product" v-on:addtocart = 'addtocart'></goods-item>
     </div>`
 })
 
+
+//компонент для карточки с товарами
 Vue.component('goods-item', {
-    props: ['goodProp'],
-    template: `<div class="goods-item">
+    props: ['goodProp', 'id'],
+    
+    template: `<div class="goods-item" v-bind:itemId='id'>
     <h3>{{goodProp.product_name}}</h3>
     <p>{{goodProp.price}}</p>
+    <button type="button" v-on:click="$emit('addtocart',$event)" class="add-btn">Добавить в корзину</button>
     </div>`
 
 })
 
-Vue.component('search-block',{
-    props: ['search',   'goods'],
-    template: `<div class="search-block">
-        <input type="text" class="goods-search" v-model="search">
-        <button v-on:click="filterGoods" class="search-button" type="button">Искать</button> 
+// компонент для блока корзины
+Vue.component('cart__goods-list', {
+    props: ['cartgoods'],
+    template: `<div class="cart__goods-list">
+        <cart__goods-item v-for="goodEntity in cartgoods" :goodProp="goodEntity" :id="goodEntity.id_product"></cart__goods-item>
+    </div>`
+})
+
+
+//компонент для карточки с товарами в корзине
+Vue.component('cart__goods-item', {
+    props: ['goodProp', 'id'],
+    template: `<div class="cart__goods-item" :itemId='id'>
+    <h3>{{goodProp.product_name}}</h3>
+    <p>{{goodProp.price}}</p>
+    <button type="button" class="remove-btn">Удалить из корзины</button>
     </div>`
 
-    }
-)
+})
+
+
+
 
 
 
@@ -36,30 +71,32 @@ const app = new Vue({
     el: "#app",
     data: {
         goods: [],
-        filteredGoods: [{product_name:'Ноутбук', price: 46500}, {product_name:'Мышка', price: 1000}],
+        filteredGoods: [],
         searchLine: '',
         // массив с товарами в корзине
-        cartGoods: [{product_name: 'Телефон', price: 100}],
+        cartGoods: [{ product_name: 'Телефон', price: 100 }],
         // свойство управляющие видимостью корзины
         isVisibleCart: true,
         // свойство для вывода сообщения
         // если массив  с товарами пуст
-        emptyMessage: 'Список товаров пуст!'
+        emptyMessage: 'Список товаров пуст!',
+        // параметр для корзины с товарами
+        cartGoods: [],
     },
 
     methods: {
-       
+
         //получает товары с сервера
-        // async getProducts() {
-        //     const responce = await fetch(`${API_URL}/catalogData.json`);
-        //     if (responce.ok) {
-        //         const catalogItems = await responce.json();
-        //         this.goods = catalogItems;
-        //         this.filteredGoods = catalogItems;
-        //     } else {
-        //         alert("Ошибка при соединении с сервером");
-        //     }
-        // },
+        async getProducts() {
+            const responce = await fetch(`${API_URL}/catalogData.json`);
+            if (responce.ok) {
+                const catalogItems = await responce.json();
+                this.goods = catalogItems;
+                this.filteredGoods = catalogItems;
+            } else {
+                alert("Ошибка при соединении с сервером");
+            }
+        },
 
         // осуществляет поиск товаров в массиве с ними
         // по запросу в строке поиска
@@ -71,13 +108,28 @@ const app = new Vue({
             this.filteredGoods = this.goods.filter(good => regExp.test(good.product_name));
         },
 
+        addToCart(event) {
+            const idProduct = Number(event.target.parentElement.getAttribute('itemId')); // itemId карточки
+            // проходимся циклом по массиву с товарами
+            for (let item of this.filteredGoods) {
+                if (idProduct === item.id_product) { // если id кликнутой карточки и товара в массиве совпадают
+                    this.cartGoods.push(item); // добавляем товар в корзину
+                }
+            }
 
-       
+            // this.removeButtons = document.querySelectorAll('.remove-btn'); // получаем кнопки на карточках корзины
+            // this.removeFromCart(this.removeButtons); // вызываем метод по удалению этих карточек при клике на кнопку
+            // // иначе никак не получается сделать 
+            // this.countCart(this.cartGoods); // вызывам метод подсчета количества товаров в корзине их стоимсоть 
+        }
+
+
+
     },
 
-    // async mounted() {
-    //     await this.getProducts()
-    // }    
+    async mounted() {
+        await this.getProducts()
+    }    
 });
 
 
@@ -101,14 +153,14 @@ const app = new Vue({
 
 
 
-
+// { product_name: 'Ноутбук', price: 46500 }, { product_name: 'Мышка', price: 1000 }
 
 
 
 
 
 // /*
-    
+
 
 
 // */
